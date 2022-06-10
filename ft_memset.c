a8ef45e580a5d948b2611b3c2eb6c79796ce238f
issaomori <gabrielmori16@gmail.com>
2022-06-10 18:21:02 -0300

    Att_06_10

--- ft_memset.c
+++ ft_memset.c
@@ -6,15 +6,15 @@
 /*   By: gissao-m <gissao-m@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/30 14:26:15 by gissao-m          #+#    #+#             */
-/*   Updated: 2022/06/08 16:54:59 by gissao-m         ###   ########.fr       */
+/*   Updated: 2022/06/10 12:52:00 by gissao-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	*ft_memset(void *str, int c, size_t n)
+void	*ft_memset(void *ptr, int c, size_t n)
 {
 	while (n--)
-		*(unsigned char *)(str + n) = (unsigned char)(c);
-	return (str);
+		*(unsigned char *)(ptr + n) = (unsigned char)(c);
+	return (ptr);
 }
--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -6,23 +6,26 @@
 /*   By: gissao-m <gissao-m@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/01 16:51:07 by gissao-m          #+#    #+#             */
-/*   Updated: 2022/06/08 17:04:50 by gissao-m         ###   ########.fr       */
+/*   Updated: 2022/06/10 17:05:44 by gissao-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-size_t	ft_strlcpy(char *dest, const char *src, size_t size)
+size_t	ft_strlcpy(char *dst, const char *src, size_t d_size)
 {
-	size_t	i;
+	size_t	count_size;
+	size_t	l_src;
 
-	i = ft_strlen(src);
-	if (i + 1 < size)
-		ft_memmove(dest, src, i + 1);
-	else if (size != 0)
+	l_src = ft_strlen(src);
+	count_size = 0;
+	if (d_size == 0)
+		return (l_src);
+	while (src[count_size] != '\0' && count_size < (d_size - 1))
 	{
-		ft_memmove(dest, src, size - 1);
-		dest [size - 1] = '\0';
+		dst[count_size] = src[count_size];
+		count_size++;
 	}
-	return (i);
+	dst[count_size] = '\0';
+	return (l_src);
 }
